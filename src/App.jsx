6213da5f0b5cc1da4e0dc1e996e98eb89ae5b7ce
VolodymyrVorona <volodimir.vorona@gmail.com>
2021-03-15 01:49:19 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import Form from './components/Form';
+import Form from './components/Form/form';
 
 export default class App extends Component {
   formSubmitHandler = data => {
     setTimeout(() => {
       console.log(data);
     }, 1000);
+
   };
+
   render() {
     return (
       <div>
